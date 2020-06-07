dc2f2133e796808016facc0babbbd5b2d1b24d53
ddrum5 <you@example.com>
2020-06-07 15:11:03 +0700

    Full Final Final

--- src/Bai_7/Main.java
+++ src/Bai_7/Main.java
@@ -9,12 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<ThiSinh> dsTS = new ArrayList<>();
-        ThiSinh ts = new ThiSinh() {
-            @Override
-            public double tongDiem() {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
+        ThiSinh ts = null;
         System.out.print("Nhập số lượng thí sinh: ");
         int n = sc.nextInt();
         
